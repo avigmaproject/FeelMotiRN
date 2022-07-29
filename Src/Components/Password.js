08234f69aeb9b86a9d96782937dd369146ff1e5c
Mike Brown <avigma.project@gmail.com>
2022-07-29 18:23:54 +0530

    api

--- Src/Components/Password.js
+++ Src/Components/Password.js
@@ -15,11 +15,24 @@ import Header from "../CustomComponent/Header";
 import InputText from "../CustomComponent/InputText";
 import { TextInput } from "react-native-paper";
 import Button from "../CustomComponent/Button";
+import { resetpassword } from "../Utils/apiconfig";
+import { useSelector, useDispatch } from "react-redux";
+import { signout } from "../store/action/auth/action";
 const Password = ({ navigation }) => {
   const [secureTextEntry, setsecureTextEntry] = useState(true);
   const [csecureTextEntry, csetsecureTextEntry] = useState(true);
   const [osecureTextEntry, setosecureTextEntry] = useState(true);
-
+  const profile = useSelector((state) => state.profileReducer.profile);
+  const [loading, setloading] = useState(false);
+  const [message, setmessage] = useState("");
+  const [color, setcolor] = useState("green");
+  const [error, setError] = useState("");
+  const [visible, setVisible] = useState(false);
+  const onToggleSnackBar = () => setVisible(!visible);
+  const dispatch = useDispatch();
+  const onLogout = () => {
+    dispatch(signout());
+  };
   const handleSecureEntry = () => {
     setsecureTextEntry(!secureTextEntry);
   };
@@ -38,6 +51,110 @@ const Password = ({ navigation }) => {
     confirmPassword: "",
   });
   const { oldPassword, NewPassword, confirmPassword } = form;
+
+  const passwordLength = () => {
+    let cancel = false;
+    if (NewPassword.length < 8) {
+      cancel = true;
+    }
+    console.log("cancel brfore", cancel);
+
+    if (cancel) {
+      console.log("cancel after", cancel);
+      return updateError(
+        "Password length should not be less then 8.",
+        setError
+      );
+    } else {
+      return true;
+    }
+  };
+  const passwordEmpty = () => {
+    let cancel = false;
+    if (NewPassword.length === 0) {
+      cancel = true;
+    }
+
+    if (cancel) {
+      return updateError("Fields can not be empty.", setError);
+    } else {
+      return true;
+    }
+  };
+  const passwordEqual = () => {
+    let cancel = false;
+    if (NewPassword !== confirmPassword) {
+      cancel = true;
+    }
+    if (cancel) {
+      return updateError(
+        "Password and confirm password does not match.",
+        setError
+      );
+      return false;
+    } else {
+      return true;
+    }
+  };
+  const updateError = (error, stateUpdate) => {
+    stateUpdate(error);
+    setTimeout(() => {
+      stateUpdate("");
+    }, 2500);
+  };
+
+  const submitForm = async () => {
+    console.log("form", form);
+    if (passwordLength() && passwordEmpty() && passwordEqual()) {
+      let data = {
+        User_Email: profile.email,
+        Type: 5,
+        User_Password: form.NewPassword,
+        User_Type: 1,
+      };
+      console.log(data);
+      setloading(true);
+      console.log("form1", form);
+      await resetpassword(data)
+        .then((res) => {
+          setloading(false);
+          console.log("res: ", JSON.stringify(res));
+          onToggleSnackBar();
+          setmessage(
+            "Your password has been changed successfully. Use your new password to login."
+          );
+          setForm({
+            NewPassword: "",
+            confirmPassword: "",
+          });
+          setcolor("green");
+          const a = setTimeout(() => {
+            onLogout();
+          }, 2000);
+        })
+        .catch((error) => {
+          if (error.request) {
+            setmessage("Request Error");
+            setcolor("red");
+            onToggleSnackBar();
+            console.log(error.request);
+          } else if (error.responce) {
+            setmessage("Responce Error");
+            setcolor("red");
+            onToggleSnackBar();
+            console.log(error.responce);
+          } else {
+            setmessage("Somthing went wrong....");
+            setcolor("red");
+            onToggleSnackBar();
+            console.log(error);
+          }
+          setloading(false);
+        });
+      setloading(false);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#f8f8f8" }}>
       <StatusBar barStyle="dark-content" backgroundColor={"#f8f8f8"} />
@@ -102,13 +219,8 @@ const Password = ({ navigation }) => {
             }
           />
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate("AddNewPost")}>
-          <View>
-            <Text>heelo</Text>
-          </View>
-        </TouchableOpacity>
         <View style={{ paddingHorizontal: 10 }}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity style={styles.button} onPress={() => submitForm()}>
             <Text style={styles.submit}>Save Changes</Text>
           </TouchableOpacity>
         </View>
--- Src/Components/ResetPassword.js
+++ Src/Components/ResetPassword.js
@@ -7,7 +7,7 @@ import {
   Image,
   Keyboard,
   ScrollView,
-StatusBar
+  StatusBar,
 } from "react-native";
 import React, { useState } from "react";
 import { TextInput } from "react-native-paper";
@@ -15,11 +15,11 @@ import InputText from "../CustomComponent/InputText";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../CustomComponent/Header";
 import Button from "../CustomComponent/Button";
-import {resetpassword} from "../Utils/apiconfig"
-import Spinner from 'react-native-loading-spinner-overlay';
-import {  Snackbar } from 'react-native-paper';
+import { resetpassword } from "../Utils/apiconfig";
+import Spinner from "react-native-loading-spinner-overlay";
+import { Snackbar } from "react-native-paper";
 
-const ResetPassword = ({ navigation ,...props}) => {
+const ResetPassword = ({ navigation, ...props }) => {
   const [resetPassword, setResetPassword] = useState({
     password: "",
     confirmpassword: "",
@@ -32,9 +32,9 @@ const ResetPassword = ({ navigation ,...props}) => {
 
   const [email, setemail] = useState("");
   const [loading, setloading] = useState(false);
-  const [visible, setVisible] = useState(false);
-  const [color, setcolor] = useState("green")
-  const [message, setmessage] = useState("")
+
+  const [color, setcolor] = useState("green");
+  const [message, setmessage] = useState("");
   const { password, confirmpassword } = resetPassword;
 
   const onToggleSnackBar = () => setVisible(!visible);
@@ -50,7 +50,7 @@ const ResetPassword = ({ navigation ,...props}) => {
     console.log(value, fieldName);
     setResetPassword({ ...resetPassword, [fieldName]: value });
   };
-const updateError = (error, stateUpdate) => {
+  const updateError = (error, stateUpdate) => {
     stateUpdate(error);
     setTimeout(() => {
       stateUpdate("");
@@ -58,112 +58,116 @@ const updateError = (error, stateUpdate) => {
   };
 
   React.useEffect(() => {
-    const {link} = props.route.params;
-    const spliturl = link.split('/');
-    console.log('spliturl', spliturl[4]);
-    setemail(spliturl[4])
-  }, [email])
-  
-const passwordEmpty = () => {
+    const { link } = props.route.params;
+    const spliturl = link.split("/");
+    console.log("spliturl", spliturl[4]);
+    setemail(spliturl[4]);
+  }, [email]);
+
+  const passwordEmpty = () => {
     let cancel = false;
     if (password.length === 0) {
       cancel = true;
     }
-    
+
     if (cancel) {
       return updateError("Fields can not be empty.", setError);
     } else {
       return true;
     }
   };
-const passwordLength = () => {
+  const passwordLength = () => {
     let cancel = false;
     if (password.length < 8) {
       cancel = true;
     }
-    console.log("cancel brfore",cancel)
+    console.log("cancel brfore", cancel);
 
     if (cancel) {
-    console.log("cancel after",cancel)
-      return updateError('Password length should not be less then 8.', setError);
+      console.log("cancel after", cancel);
+      return updateError(
+        "Password length should not be less then 8.",
+        setError
+      );
     } else {
       return true;
     }
   };
-const passwordEqual = () => {
+  const passwordEqual = () => {
     let cancel = false;
     if (password !== confirmpassword) {
       cancel = true;
     }
     if (cancel) {
-      return updateError('Password and confirm password does not match.', setError);
+      return updateError(
+        "Password and confirm password does not match.",
+        setError
+      );
       return false;
     } else {
       return true;
     }
   };
-const submitForm = async () => {
-    if (
-      passwordLength() &&
-      passwordEmpty() && passwordEqual() 
-    ) {   
+  const submitForm = async () => {
+    if (passwordLength() && passwordEmpty() && passwordEqual()) {
+      let data = {
+        User_Email: email,
+        Type: 5,
+        User_Password: password,
+        User_Type: 1,
+      };
+      console.log(data);
+      setloading(true);
 
-        let data = {
-          User_Email: email,
-          Type: 5,
-          User_Password: password,
-          User_Type: 1,
-        };
-        console.log(data);
-    setloading(true); 
-
-        await resetpassword(data)
-          .then(res => {
-           setloading(false); 
-           console.log('res: ', JSON.stringify(res));
-           onToggleSnackBar()
-           setmessage("Your password has been changed successfully. Use your new password to login.") 
+      await resetpassword(data)
+        .then((res) => {
+          setloading(false);
+          console.log("res: ", JSON.stringify(res));
+          onToggleSnackBar();
+          setmessage(
+            "Your password has been changed successfully. Use your new password to login."
+          );
           setResetPassword({
             password: "",
-            confirmpassword: "",})
-           setcolor("green")
-            const a = setTimeout(() => { navigation.navigate("Signin")}, 2000);
-
-          })
-          .catch(error => {
-            if (error.request) {
-                setmessage("Request Error") 
-                setcolor("red")
-                onToggleSnackBar()
-                console.log(error.request)
-              } else if (error.responce) {
-                setmessage("Responce Error") 
-                setcolor("red")
-                onToggleSnackBar()
-                console.log(error.responce)
-              } else {
-                setmessage("Somthing went wrong....") 
-                setcolor("red")
-                onToggleSnackBar()
-                console.log(error)
-              }
-           setloading(false); 
-
+            confirmpassword: "",
           });
-                 setloading(false); 
-
+          setcolor("green");
+          const a = setTimeout(() => {
+            navigation.navigate("Signin");
+          }, 2000);
+        })
+        .catch((error) => {
+          if (error.request) {
+            setmessage("Request Error");
+            setcolor("red");
+            onToggleSnackBar();
+            console.log(error.request);
+          } else if (error.responce) {
+            setmessage("Responce Error");
+            setcolor("red");
+            onToggleSnackBar();
+            console.log(error.responce);
+          } else {
+            setmessage("Somthing went wrong....");
+            setcolor("red");
+            onToggleSnackBar();
+            console.log(error);
+          }
+          setloading(false);
+        });
+      setloading(false);
     }
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
       <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-      <Spinner
-          visible={loading}
-          textContent={'Loading...'}
-        />
+      <Spinner visible={loading} textContent={"Loading..."} />
       <Header color={true} onPress={() => navigation.navigate("Signin")} />
-      <ScrollView keyboardShouldPersistTaps={"always"} contentContainerStyle={{ flexGrow: 1, marginHorizontal: 20 }}>
+      <ScrollView
+        keyboardShouldPersistTaps={"always"}
+        contentContainerStyle={{ flexGrow: 1, marginHorizontal: 20 }}
+      >
         <View>
           <View style={styles.heading}>
             <Text style={styles.text}>Reset Password</Text>
@@ -200,7 +204,6 @@ const submitForm = async () => {
                 />
               }
               error1={error1}
-
             />
           </View>
           <View>
@@ -210,13 +213,16 @@ const submitForm = async () => {
         <Snackbar
           visible={visible}
           onDismiss={onDismissSnackBar}
-          style={{backgroundColor:color}}
+          style={{ backgroundColor: color }}
           action={{
-            label: 'OK',
+            label: "OK",
             onPress: () => {
-              onDismissSnackBar
+              onDismissSnackBar;
             },
-          }}>{message}</Snackbar>
+          }}
+        >
+          {message}
+        </Snackbar>
       </ScrollView>
     </SafeAreaView>
   );
